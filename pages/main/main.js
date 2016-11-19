a6c0fe5a1efea9a2e7f06c6378064d8b69d8d722
Tayoji <595521651@qq.com>
2016-11-19 17:54:22 +0800

    ＃增加＃工具方法

--- pages/main/main.js
+++ pages/main/main.js
@@ -1,31 +1,59 @@
+var util = require('../../utils/util.js')
 Page({
-  data:{
-    
+  data: {
+
   },
-  onLoad:function(options){
+  onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
     wx.setNavigationBarTitle({
       title: '首页',
-      color:'#ff00ff',
-      success: function(res) {
+      color: '#ff00ff',
+      success: function (res) {
         // success
       }
     })
+    let code = "Basic " + util.base64encode("xiucheren-client-owner-ios:1a2730fc771f359db9ebe14b45b02705")
+    console.log(code)
+    wx.request({
+      url: 'https://api.xiucheren.net/oauth/token',
+      data: {
+        grant_type: "client_credentials",
+      },
+      method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      header: {
+        "Authorization": code,
+        "Content-Type":"application/x-www-form-urlencoded",
+        "Accept":"application/json"
+      }, // 设置请求的 header
+      success: function (res) {
+        // success
+        console.log(res)
+      },
+      fail: function () {
+        console.log('fail')
+
+      },
+      complete: function () {
+        // complete
+        console.log('complete')
+
+      }
+    })
   },
-  onReady:function(){
+  onReady: function () {
     // 页面渲染完成
-    
+
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
-    
+
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
-    
+
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
-    
+
   }
 })
