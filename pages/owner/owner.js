00f47724cfcb31f68ae1f7f23e26b2e08d1267e0
tangyoujian <tangyoujian@xiucheren.net>
2016-11-19 16:44:14 +0800

    ＃增加＃相关目录

---
+++ pages/owner/owner.js
@@ -0,0 +1,29 @@
+Page({
+  data:{
+    
+  },
+  onLoad:function(options){
+   wx.setNavigationBarTitle({
+     title: '个人中心',
+     success: function(res) {
+       // success
+     }
+   })
+  },
+  onReady:function(){
+    // 页面渲染完成
+    
+  },
+  onShow:function(){
+    // 页面显示
+    
+  },
+  onHide:function(){
+    // 页面隐藏
+    
+  },
+  onUnload:function(){
+    // 页面关闭
+    
+  }
+})
