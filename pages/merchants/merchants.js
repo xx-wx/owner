6e90424aaa5d8c38473cc367a13dc983dff752af
cadaverousblue <changbo89757@sina.com>
2016-11-22 13:54:28 +0800

    列表数据请求
    i do it but no use

--- pages/merchants/merchants.js
+++ pages/merchants/merchants.js
@@ -1,15 +1,25 @@
+let API = require("../../utils/Api.js")
 Page({
     data: {
-
+        mapIcon: "../../resources/tabbar/1-selected.png",
+        msgIcon: "../../resources/tabbar/2-selected.png",
+        placehodler: "搜索配件",
+        merchants: [],
+        isLoading: true
     },
     onLoad: function (options) {
         // 页面初始化 options为页面跳转所带来的参数
         wx.setNavigationBarTitle({
             title: '商家',
-            success: function (res) {
-                // success
-            }
+
         })
+
+        wx.showToast({
+            title: "加载中",
+            icon: "loading",
+            duration: 10000,
+        })
+        this.loadData()
     },
     onReady: function () {
         // 页面渲染完成
@@ -26,5 +36,34 @@ Page({
     onUnload: function () {
         // 页面关闭
 
+    },
+    loadData: function () {
+        let parms = {
+            "mapCoordinates": "113.692797,34.806201",
+            "keyword": "",
+            "category": "all",
+            "orderType": "default",
+            "pageNo": 1,
+            "cityCode": 268,
+            "areaCode": -1,
+            "insuranceLevel": 1,
+            "shopType": ""
+        }
+        let that = this
+        API.POST(API.Url("/owner/serviceShop/list.jhtml"), parms, function (data) {
+            console.log(data)
+            wx.hideToast()
+            console.log(this)
+            that.setData({
+                isLoading:false,
+                merchants: data.shopList
+            })
+        }, function (error) {
+            console.log(error)
+        })
     }
+
+
+
+
 })
